1af6ab090d85d6ff4091f309974f36ab4c3841a7
Chris Pelatari <chris@pelatari.com>
2023-01-28 04:57:34 -0600

    Update pack.ps1

--- pack/pack.ps1
+++ pack/pack.ps1
@@ -4,17 +4,17 @@
 
 Copy-Item -Path ..\src\Areas -Destination .\Areas -Recurse -Force
 
-Get-ChildItem -Path *.cs -Recurse -File | foreach {
+Get-ChildItem -Path *.cs -Recurse -File | ForEach-Object {
     Rename-Item $_ ($_.FullName + ".pp") -Force
 }
 
-Get-ChildItem -Path *.cshtml -Recurse -File | foreach {
+Get-ChildItem -Path *.cshtml -Recurse -File | ForEach-Object {
     Rename-Item $_ ($_.FullName + ".pp") -Force
 }
 
-Get-ChildItem -Path *.cs.pp -Recurse -File | foreach {
+Get-ChildItem -Path *.cs.pp -Recurse -File | ForEach-Object {
     $content = Get-Content -Path $_
-    $content | foreach { $_ -replace "RouteDebugger.Areas", '$rootnamespace$.Areas' } | Set-Content $_
+    $content | ForEach-Object { $_ -replace "RouteDebugger.Areas", '$rootnamespace$.Areas' } | Set-Content $_
 }
 
-..\src\.nuget\NuGet.exe pack WebApiRouteDebugger.nuspec
+..\src\.nuget\NuGet.exe pack BlueFenix.WebApiRouteDebugger.nuspec
